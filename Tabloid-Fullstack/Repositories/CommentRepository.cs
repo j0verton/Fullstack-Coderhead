07d161124fbfe519e18a187d88ca21cc3aec584a
Joe <overtonje@gmail.com>
2021-01-19 11:22:14 -0600

    two bypostid and add methods in comment repo

--- Tabloid-Fullstack/Repositories/CommentRepository.cs
+++ Tabloid-Fullstack/Repositories/CommentRepository.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Tabloid_Fullstack.Data;
+using Tabloid_Fullstack.Models;
 
 namespace Tabloid_Fullstack.Repositories
 {
@@ -14,5 +16,29 @@ namespace Tabloid_Fullstack.Repositories
         {
             _context = context;
         }
+
+        public List<Comment> GetCommentsByPostId(int id)
+        {
+            return _context.Comment
+                .Include(c => c.User)
+                    .Where(c => c.PostId == id)
+                    .ToList();
+        }
+
+        public void Add(Comment comment)
+        {
+            try {
+
+                _context.Add(comment);
+                _context.SaveChanges();
+
+
+            }
+            catch (Exception ex)
+            { 
+            
+            }
+        }
+
     }
 }
