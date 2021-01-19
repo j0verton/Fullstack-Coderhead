85f623bc91c3a5d3320051d2cc69368e85d89a56
Joe <overtonje@gmail.com>
2021-01-19 11:13:31 -0600

    build comment model, working on comment repo

---
+++ Tabloid-Fullstack/Repositories/CommentRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tabloid_Fullstack.Data;
+
+namespace Tabloid_Fullstack.Repositories
+{
+    public class CommentRepository
+    {
+        private ApplicationDbContext _context;
+
+        public CommentRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
