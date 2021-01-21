1392ba023f65581286862adf3712d489f6815b5d
Joe <overtonje@gmail.com>
2021-01-20 21:13:10 -0600

    adding userId to PostReaction model

--- Tabloid-Fullstack/client/src/pages/PostDetails.js
+++ Tabloid-Fullstack/client/src/pages/PostDetails.js
@@ -15,7 +15,7 @@ const PostDetails = () => {
   const [comments, setComments] = useState([]);
 
   const getPost = () => {
-    fetch(`/api/post/${postId}`)
+    return fetch(`/api/post/${postId}`)
       .then((res) => {
         if (res.status === 404) {
           toast.error("This isn't the post you're looking for");
@@ -24,6 +24,7 @@ const PostDetails = () => {
         return res.json();
       })
       .then((data) => {
+        console.log(data)
         setPost(data.post);
         setReactionCounts(data.reactionCounts);
         setComments(data.comments)
