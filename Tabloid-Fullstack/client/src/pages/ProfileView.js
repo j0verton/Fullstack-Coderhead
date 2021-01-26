3a5e35434a0403194c2086fda1e899571ecdef13
Joe <overtonje@gmail.com>
2021-01-26 11:45:15 -0600

    writing upload method for firbase

--- Tabloid-Fullstack/client/src/pages/ProfileView.js
+++ Tabloid-Fullstack/client/src/pages/ProfileView.js
@@ -2,18 +2,19 @@ import React, { useEffect, useState, useContext } from "react"
 import { Input, Progress } from "reactstrap";
 import ProfileSummaryCard from "../components/UserProfile/ProfileSummaryCard"
 import { UserProfileContext } from "../providers/UserProfileProvider";
+import firebase from "firebase/app";
 
 const ProfileView = () => {
 
     const { getToken } = useContext(UserProfileContext);
-    const [profile, setProfile] = useState();
+    const [profile, setProfile] = useState({ displayName: "", userType: { name: "" }, createDateTime: new Date(), firebaseUserId: localStorage.getItem("firebaseUserId") });
     const [isLoading, setIsLoading] = useState(false);
+    const [loadingProgress, setLoadingProgress] = useState(0);
 
     useEffect(() => {
         getCurrentUserProfile()
 
     }, [])
-
 
     const getCurrentUserProfile = () => {
         getToken()
@@ -23,22 +24,62 @@ const ProfileView = () => {
                     headers: {
                         Authorization: `Bearer ${token}`,
                     },
-                }))
-
-            .then((res) => res.json())
-            .then((profile) => {
-                setProfile(profile)
-            });
+                })
+                    .then(res => res.json())
+                    .then(profile => {
+                        console.log(profile)
+                        setProfile(profile)
+                    }));
     }
-    return (
-        <div>
-            <ProfileSummaryCard profile={profile} />
-            {
-                isLoading ?
-                    <Progress /> : null
+
+    const uploadImage = async e => {
+        setLoading(true)
+        const file = e.target.files[0]
+        let storageRef = firebase.storage().ref(`ProfilePictures/${file.name}`)
+        let task = storageRef.put(file)
+        task.on('state_changed',
+            function progess(snapshot) {
+
+                let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                setLoadingProgress(percentage)
             }
-            <Input type="file" value="Upload a Profile Picture" id="profilePicUpload" />
-        </div>
+        )
+        const data = new FormData()
+        data.append('file', files[0])
+        data.append('upload_preset', "tunelist")
+
+        const response = await fetch(
+            'https://api.cloudinary.com/v1_1/banjo/image/upload',
+            {
+                method: 'POST',
+                body: data
+            }
+        )
+        const responseImage = await response.json()
+        await addPhoto(responseImage.url)
+        // let photos = await getPhotosByUserId(localStorage.getItem("tunes_user")) 
+        // setImages(photos)
+        setLoading(false)
+    }
+
+    return (
+        <>
+            {
+
+                <div>
+                    <ProfileSummaryCard profile={profile} />
+                    {
+                        isLoading ?
+                            <Progress /> : null
+                    }
+                    <Input
+                        type="file"
+                        id="profilePicUpload"
+                        placeholder="Upload a Profile Picture"
+                        onChange={uploadImage} />
+                </div>
+            }
+        </>
     )
 }
 export default ProfileView;
